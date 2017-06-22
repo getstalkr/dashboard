b87824ff2ca55f52cc4c7325556eea4f3a6d4433
Matheus Brasil <matheus.brasil10@gmail.com>
2017-06-22 11:47:11 -0300

    chore(tasks): add deploying and serving tasks

---
+++ tasks/serve/index.js
@@ -0,0 +1,31 @@
+const webpack = require('webpack')
+const WebpackDevServer = require('webpack-dev-server')
+const config = require('../../webpack/dev')
+
+const PORT = 3001
+
+new WebpackDevServer(webpack(config), {
+
+  publicPath: config.output.publicPath,
+  hot: true,
+  inline: false,
+  historyApiFallback: true,
+  quiet: false,
+  // It suppress everything except error, so it has to be set to false as well
+  // to see success build.
+  noInfo: false,
+  stats: {
+    assets: false,
+    colors: true,
+    version: false,
+    hash: false,
+    timings: false,
+    chunks: false,
+    chunkModules: false
+  }
+}).listen(PORT, 'localhost', function (error, result) {
+  if (error) {
+    console.log(error)
+  }
+  console.log(`Listening at http://localhost:${PORT}`)
+})
