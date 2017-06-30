c2ac156b7536ce8fb654afd243bba1c72241a8ec
Matheus Brasil <matheus.brasil10@gmail.com>
2017-06-30 14:11:45 -0300

    {design, code} in (general): wrap everything

---
+++ src/components/App.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import Nav from './common/Nav';
+
+class App extends Component {
+  render () {
+    return (
+      <div className='wrapper'>
+        <Nav loggedIn={this.props.data.loggedIn}
+          currentlySending={this.props.data.currentlySending}
+          history={this.props.history}
+          dispatch={this.props.dispatch}
+          location={this.props.location} />
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
+App.propTypes = {
+  data: PropTypes.object,
+  history: PropTypes.object,
+  location: PropTypes.object,
+  children: PropTypes.object,
+  dispatch: PropTypes.func
+}
+
+function select (state) {
+  return {
+    data: state
+  }
+}
+
+export default connect(select)(App)
